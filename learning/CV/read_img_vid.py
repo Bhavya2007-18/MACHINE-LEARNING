a37bbc3b90fcd678dae531606084d65d0fad1f70
bhavya porwal <bhavyaporwal007@gmail.com>
2026-06-29 23:07:06 +0530

    seted up gitignore and venv

--- learning/CV/read_img_vid.py
+++ learning/CV/read_img_vid.py
@@ -1,26 +1,36 @@
 import cv2
 import mediapipe as mp
 import time
-# to see an image
-img = cv2.imread("/home/bhavyaporwal007/work and study/MACHINE-LEARNING/learning/CV/resources/hand.jpg")
 
-cv2.imshow("hand" , img)
-cv2.waitKey(0)
+# Load an image
+img = cv2.imread("D:/WORK AND STUDY/projects/MACHINE-LEARNING/learning/CV/assets/hand.jpg")
 
-#to run the webcam
+if img is None:
+    print("Image not found. Check the path spelling!")
+else:
+    cv2.imshow("hand", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-#1. define the frame size
+# Run the webcam
 framewidth = 640
 frameheight = 360  
 
-cap=cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+cap.set(3, framewidth)   # set width
+cap.set(4, frameheight)  # set height
+
 while True:
     success, img = cap.read()
+    if not success:
+        print("Failed to grab frame")
+        break
 
-    cv2.imshow("webcam" , img)
-    # press q to exit   
+    cv2.imshow("webcam", img)
 
+    # press q to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
-       break
+        break
 
 cap.release()
+cv2.destroyAllWindows()
